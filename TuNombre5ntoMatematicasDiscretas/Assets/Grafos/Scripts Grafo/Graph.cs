7d4ad0a8eb97bc45552adfb2849885bf45db4a53
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-08-28 15:50:06 -0600

    1

--- TuNombre5ntoMatematicasDiscretas/Assets/Grafos/Scripts Grafo/Graph.cs
+++ TuNombre5ntoMatematicasDiscretas/Assets/Grafos/Scripts Grafo/Graph.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using System.Collections.Generic;
+
+//queu primero es su indice y leugo es su costo 
 
 public class Graph : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
