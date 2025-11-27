9104ef99bf3c5e751f5b6118a297218d3576c2a8
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-11-27 15:58:43 -0600

    permutaciones y conbinaciones

---
+++ TuNombre5ntoMatematicasDiscretas/Assets/Scenes/Colecciones Genericas/EjerciciosPermutaciones y conbinaciones.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EjerciciosPermutacionesyconbinaciones : MonoBehaviour
+{
+    List<string> stringList = new List<string> {"A","B","C","D" };
+    void Start()
+    {
+        Permutaciones();
+        //Conbinaciones();
+    }
+    public void Permutaciones()
+    {
+        List<List<string>> permutacionesList = new List<List<string>>();
+        
+        for (int i = 0; i < stringList.Count; i++)
+        {
+          
+        }
+    }
+    public void Conbinaciones()
+    {
+
+    }
+}
